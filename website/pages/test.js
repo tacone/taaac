374cae5214a5fcabb3672c8fdb20808f6a49c505
tacone <tacone@gmail.com>
2020-07-17 00:48:17 +0200

    new containers, refactoring
    - sample next.js installation in place of plain node
    - add matomo for tracking, still unlinked and untested
    - use node-alpine instead of plain node
    - use postgres-alpine instead of plain postgres
    - remove /.gitignore, it will come back some day

---
+++ website/pages/test.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Ciao!</h1>
+      </main>
+    </div>
+  );
+}
