d87d34751e6c0f69bf62496e98380e8dacb5c3fd
tacone <tacone@gmail.com>
2020-07-21 01:06:48 +0200

    fix matomo, add it to test.js, it will not work unless you disable donottrack

--- website/pages/test.js
+++ website/pages/test.js
@@ -1,15 +1,18 @@
 import Head from "next/head";
+import Layout from "layout";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Ciao!</h1>
-      </main>
-    </div>
+    <Layout>
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <h1>Ciao!</h1>
+        </main>
+      </div>
+    </Layout>
   );
 }
