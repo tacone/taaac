f067376f661ab8f5aecc5c8c737c8acc9345fcd0
tacone <tacone@gmail.com>
2020-11-07 00:08:25 +0100

    switch sapper on rollup

--- sapper/src/routes/index.svelte
+++ sapper/src/routes/index.svelte
@@ -1,66 +1,50 @@
-<svelte:head>
-  <title>Sapper project template</title>
-</svelte:head>
-
-{#if typeof user != 'undefined'}
-  <h1>Great success!</h1>
-{/if}
-cia
-<p>count: {counter}, double: {double}</p>
-<p><input type="button" value="get success" on:click={setUser} /></p>
-<figure>
-  <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-  <strong>Try editing this file (src/routes/index.svelte) to test live
-    reloading.</strong>
-</p>
-
 <script>
-  let user;
-  let counter = 0;
-  $: double = counter * 2;
-
-  const setUser = () => {
-    user = 1;
-    counter++;
-  };
+	import successkid from 'images/successkid.jpg';
 </script>
 
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
 
-  figure {
-    margin: 0 0 1em 0;
-  }
+	figure {
+		margin: 0 0 1em 0;
+	}
 
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
 
-  p {
-    margin: 1em auto;
-  }
+	p {
+		margin: 1em auto;
+	}
 
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
 </style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
--- sapper/src/server.js
+++ sapper/src/server.js
@@ -1,11 +1,12 @@
-import sirv from "sirv";
-import polka from "polka";
-import compression from "compression";
-import * as sapper from "@sapper/server";
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === "development";
+const dev = NODE_ENV === 'development';
 
 const devServerMiddleware = dev
   ? createProxyMiddleware({
@@ -14,19 +15,19 @@ const devServerMiddleware = dev
     })
   : (req, res, next) => {
       next();
-    };
+	};
 
 polka() // You can also use Express
-  .use(
-    // prevent CORS errors on localhost
-    "__sapper__",
-    devServerMiddleware
-  )
-  .use(
-    compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware()
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log("error", err);
-  });
+	.use(
+		// prevent CORS errors on localhost
+		"__sapper__",
+		devServerMiddleware
+	)
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
