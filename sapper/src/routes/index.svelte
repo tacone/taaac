eae3b0927153c896f60ebf8d30825d843692593a
tacone <tacone@gmail.com>
2020-11-06 21:34:11 +0100

    sapper: fix dev client, fix .prettierrc

--- sapper/src/routes/index.svelte
+++ sapper/src/routes/index.svelte
@@ -1,46 +1,66 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
+{#if typeof user != 'undefined'}
+  <h1>Great success!</h1>
+{/if}
+cia
+<p>count: {counter}, double: {double}</p>
+<p><input type="button" value="get success" on:click={setUser} /></p>
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+  <img alt="Success Kid" src="successkid.jpg" />
+  <figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+  <strong>Try editing this file (src/routes/index.svelte) to test live
+    reloading.</strong>
+</p>
+
+<script>
+  let user;
+  let counter = 0;
+  $: double = counter * 2;
+
+  const setUser = () => {
+    user = 1;
+    counter++;
+  };
+</script>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
--- sapper/src/server.js
+++ sapper/src/server.js
@@ -1,17 +1,32 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+
+const devServerMiddleware = dev
+  ? createProxyMiddleware({
+      target: "http://localhost:7006",
+      changeOrigin: true,
+    })
+  : (req, res, next) => {
+      next();
+    };
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    // prevent CORS errors on localhost
+    "__sapper__",
+    devServerMiddleware
+  )
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
