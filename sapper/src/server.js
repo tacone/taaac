4fe91cd21b3ecbdfa713e3e69f6856cd3607c36a
tacone <tacone@gmail.com>
2020-11-16 23:10:10 +0100

    working nginx proxy :cool:, sapper: disable service worker, misc.

--- sapper/src/server.js
+++ sapper/src/server.js
@@ -1,12 +1,11 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
 const devServerMiddleware = dev
   ? createProxyMiddleware({
@@ -15,19 +14,19 @@ const devServerMiddleware = dev
     })
   : (req, res, next) => {
       next();
-	};
+    };
 
 polka() // You can also use Express
-	.use(
-		// prevent CORS errors on localhost
-		"__sapper__",
-		devServerMiddleware
-	)
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    // prevent CORS errors on localhost
+    "__sapper__",
+    devServerMiddleware
+  )
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
